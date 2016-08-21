Start with correct calculation for activity coefficient of Deby-Hueckel and Davies eq.

homogeneous_reaction_set_equilibrium.py
@@ -1077,7 +1077,7 @@ class UiGroupBox(QtGui.QWidget):
         while not self.acceptable_solution \
                 and k < max_it and stop is False \
                 and not self.was_canceled():
-            neq, meq, xieq, gammaeq, ionic_str_eq, method_loops = \
+            neq, meq, xieq, gammaeq_iii, ionic_str_eq, method_loops = \
                 calc_xieq(
                     n0, mm, z, s_index, kc, nu_ij,
                     neq_0, xieq_0, 298.15, method, max_it, tol,
@@ -1133,7 +1133,6 @@ class UiGroupBox(QtGui.QWidget):
         rhoeq = np.multiply(ceq, mm)
         weq = np.multiply(neq, mm)
         xweq = weq / sum(weq)
-        gammaeq_iii = gammaeq
         aeq = np.multiply(gammaeq_iii, meq * 1000)
         gammaeq_ii = mm[s_index].item() * np.multiply(
             gammaeq_iii, np.multiply(

reaction_equilibrium.py
@@ -46,13 +46,15 @@ def calc_xieq(
     a_m = a_m_d_h(t_abs)
 
     meq_0 = neq_0 / (mm_0 * neq_0[s_index])
-    gammaeq_0 = np.matrix(np.ones([n, 1]))
     ionic_str_eq_0 = 1 / 2.0 * np.power(z, 2).T * meq_0
     gammaeq = np.ones([n, 1])
+    gammaeq_0 = gammaeq
     neq = neq_0
     meq = meq_0
     xieq = xieq_0
     ionic_str_eq = ionic_str_eq_0
+    m0_ref = 1 / 1000.0  # ref. 1mol/kgsolvent conv. to mol/gsolvent
+    ionic_str_eq_adim = ionic_str_eq / m0_ref
     f = partial(
         f_gl_0_ideal,
         n0=n0,
@@ -107,11 +109,11 @@ def calc_xieq(
         ordered_neq_0 = np.matrix(
             [neq_0[index].item() for index in component_order]
         ).T
-        ordered_gammaeq_0[0] = gamma_solvent_id(mm_0, ordered_meq_0[1:])
-        ordered_gammaeq_0[1:] = np.multiply(
-            gamma_davies(ordered_z[1:], ionic_str_eq_0, a_m),
-            gamma_setchenow(ordered_z[1:], ionic_str_eq_0, 0.1))
         if method == 'davies':
+            ordered_gammaeq_0[0] = gamma_solvent_id(mm_0, ordered_meq_0[1:])
+            ordered_gammaeq_0[1:] = np.multiply(
+                gamma_davies(ordered_z[1:], ionic_str_eq_adim, a_m),
+                    gamma_setchenow(ordered_z[1:], ionic_str_eq_adim, 0.1))
             f = partial(
                 f_gl_0_davies,
                 n0=ordered_n0,
@@ -133,6 +135,10 @@ def calc_xieq(
                 mm_0=mm_0,
                 a_m=a_m)
         elif method == 'debye-hueckel':
+            ordered_gammaeq_0[0] = gamma_solvent_id(mm_0, ordered_meq_0[1:])
+            ordered_gammaeq_0[1:] = np.multiply(
+                gamma_d_h(ordered_z[1:], ionic_str_eq_adim, a_m),
+                gamma_setchenow(ordered_z[1:], ionic_str_eq_adim, 0.1))
             f = partial(
                 f_gl_0_d_h,
                 n0=ordered_n0,